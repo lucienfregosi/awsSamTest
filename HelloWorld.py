96af859600292c5cdfb6f376e649960038d74b38
fregosi lucien <lfregosi@ippon.fr>
2018-01-10 13:00:21 +0100

    oubli extension...

--- HelloWorld.py
+++
@@ -1,8 +0,0 @@
-import logging
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-def hello_world(event, context):
-	logger.info('got event{}'.format(event))
-    return { "message": "Hello, World!" }
-
